5aeba4614c3037214ca4dbc20955c7f19c118310
Daniel Sinteck <d.sint3ck@gmail.com>
2024-05-09 00:08:00 +0200

    ++ finished overhaul pkmn card;

---
+++ js/html.js
@@ -0,0 +1,31 @@
+function pkmnCard(pkmnData) {
+  return /* html */ `
+    <section class="pkmn_card" style="background-color: ${pkmnColor(
+      pkmnData,
+      0
+    )}">
+      <div class="pkmn_id">${pkmnId(pkmnData)}</div>
+  
+      <div class="pkmn_info_container">
+        <div class="pkmn_info">
+          <span class="pkmn_name">${pkmnName(pkmnData)}</span>
+          <div class="pkmn_type" style="background-color: ${pkmnColor(
+            pkmnData,
+            0
+          )}">${pkmnTypes(pkmnData).primaryType}</div>
+          <div>${
+            pkmnTypes(pkmnData).secondaryType !== ""
+              ? `<div class="pkmn_type" style="background-color: ${pkmnColor(
+                  pkmnData,
+                  1
+                )}">${pkmnTypes(pkmnData).secondaryType}</div>`
+              : ""
+          } 
+          </div>
+        </div>
+  
+        <img src="${pkmnImg(pkmnData)}" alt="Pokemon" />
+     </div>
+    </section>
+      `;
+}
--- js/loadData.js
+++ js/loadData.js
@@ -1,5 +1,5 @@
 let pkmnDataArray = [];
-let count = 52;
+let count = 51;
 let startCount = 1;
 
 async function loadMore() {
@@ -19,29 +19,7 @@ async function renderPkmnData() {
     let pkmnData = await response.json();
     pkmnDataArray.push(pkmnData);
 
-    content.innerHTML += /* html */ `
-    <section class="pkmn_card_container"
-    style="background-color: ${pkmnColor(pkmnData, 0)}"
-    >
-      <div class="pkmn_id">${pkmnId(pkmnData)}</div>
-      <div class="pkmn_card">
-
-      <span>${pkmnName(pkmnData)}</span>
-
-      <div class="type1" style="background-color: ${pkmnColor(pkmnData, 0)}">
-      ${pkmnType1(pkmnData)}
-      </div>
-      
-      <div class="type2" style="background-color: ${pkmnColor(pkmnData, 1)}">
-      ${pkmnType2(pkmnData)}
-      </div>
-
-      <img src="${pkmnImg(pkmnData)}" alt="Pokemon">
-    
-      </div>
-
-    </section>
-    `;
+    content.innerHTML += pkmnCard(pkmnData);
   }
 }
 
@@ -55,7 +33,15 @@ let pkmnName = (pkmnData) => {
   return pkmnData.name.charAt(0).toUpperCase() + pkmnData.name.slice(1);
 };
 
-let capitalizeTypes = (pkmnData) => {
+let pkmnColor = (pkmnData, typeNumber) => {
+  let type = pkmnData.types[typeNumber]
+    ? pkmnData.types[typeNumber].type.name
+    : "";
+  let backgroundColor = typeColors3[type] || "#CCCCCC";
+  return backgroundColor;
+};
+
+let pkmnTypes = (pkmnData) => {
   let primaryType =
     pkmnData.types.length > 0
       ? pkmnData.types[0].type.name.charAt(0).toUpperCase() +
@@ -70,22 +56,6 @@ let capitalizeTypes = (pkmnData) => {
   return { primaryType, secondaryType };
 };
 
-let pkmnColor = (pkmnData, typeNumber) => {
-  let type = pkmnData.types[typeNumber]
-    ? pkmnData.types[typeNumber].type.name
-    : "";
-  let backgroundColor = typeColors[type] || "#CCCCCC";
-  return backgroundColor;
-};
-
 let pkmnImg = (pkmnData) => {
   return pkmnData.sprites.front_default;
-};
-
-let pkmnType1 = (pkmnData) => {
-  return pkmnData.types[0].type.name;
-};
-
-let pkmnType2 = (pkmnData) => {
-  return pkmnData.types[1] ? pkmnData.types[1].type.name : "";
 };
