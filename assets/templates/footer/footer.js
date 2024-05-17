3641437826a76a9aa312b1feaf9a9c261ace0390
Daniel Sinteck <d.sint3ck@gmail.com>
2024-05-17 23:23:38 +0200

    ++ impress;

---
+++ assets/templates/footer/footer.js
@@ -0,0 +1,7 @@
+function toggleImprint() {
+  document.getElementById("imprintContainer").classList.remove("d_none");
+}
+
+function closeImprint() {
+    document.getElementById("imprintContainer").classList.add("d_none");
+}
